2736af71fc5fd6c50dd31b060b73f3ee1a55a6e3
temevh <temehama@gmail.com>
2026-01-13 15:12:55 +0200

    tried to deploy to AWS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,8 @@ const server = new ApolloServer({
   context,
 });
 
-server.listen({ port: process.env.PORT || 5000 }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+server
+  .listen({ port: process.env.PORT || 5000, host: "0.0.0.0" })
+  .then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
